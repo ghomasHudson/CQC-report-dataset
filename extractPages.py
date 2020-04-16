7ec265abf15a4e419b374fe8f07dad97da9ced88
Thomas Hudson <g.t.hudson@durham.ac.uk>
2020-04-16 09:11:08 +0100

    Working version for first pdf

--- extractPages.py
+++ extractPages.py
@@ -10,12 +10,13 @@ from pdfminer.layout import *
 
 class TextConverter(PDFConverter):
     def __init__(self, rsrcmgr, outfp, codec='utf-8', pageno=1, laparams=None,
-                 showpageno=False, imagewriter=None):
+                 showpageno=False, imagewriter=None,to_remove=[]):
         PDFConverter.__init__(self, rsrcmgr, outfp, codec=codec, pageno=pageno,
                               laparams=laparams)
         self.showpageno = showpageno
         self.imagewriter = imagewriter
         self.is_bold = False
+        self.to_remove = to_remove
         return
 
     def write_text(self, text):
@@ -45,57 +46,7 @@ class TextConverter(PDFConverter):
 
                 bad_item = False
                 if hasattr(item, 'size'):
-                    to_remove = [
-
-                        ("SourceSansPro-Regular",10.0),
-                        ("SourceSansPro-Bold",10.0),
-                        ("SourceSansPro-Bold",13.0),
-                        ("SourceSansPro-Bold",14.0),
-                        ("SourceSansPro-Bold",14.0),
-                        ("SourceSansPro-Bold",15.0),
-                        ("SourceSansPro-Regular",12.0),
-                        ("SourceSansPro-Semibold",10.0),
-                        ("SourceSansPro-Regular",26.0),
-                        ("SourceSansPro-Light",44.0),
-                        ("SourceSansPro-Bold",15.0),
-                        ("SourceSansPro-Light",12.0),
-                        ('SourceSansPro-Light', 0.0),
-                        '''
-                        #('SourceSansPro-Light', 44.0),
-                        #('SourceSansPro-Regular', 12.0),
-                        #('SourceSansPro-Light', 11.25),
-                        ('SourceSansPro-Regular', 22.0),
-                        ('SourceSansPro-Regular', 16.0),
-                        ('SourceSansPro-Regular', 18.0),
-                        #('SourceSansPro-Bold', 18.0),
-                        ('SourceSansPro-Bold', 16.0),
-                        ('SourceSansPro-Regular', 15.000000000000057),
-                        ('SourceSansPro-Regular', 15.0),
-                        #('SourceSansPro-Bold', 13.0),
-                        #('SourceSansPro-Bold', 14.0),
-                        ('SourceSansPro-Light', 11.250000000000014),
-                        #('SourceSansPro-Bold', 10.0),
-                        #('SourceSansPro-Regular', 10.0),
-                        #('SourceSansPro-Regular', 26.0),
-                        ('SourceSansPro-Bold', 11.25),
-                        ('SourceSansPro-Light', 11.249999999999972),
-                        #('SourceSansPro-Bold', 15.0),
-                        ('SourceSansPro-Regular', 11.0),
-                        ('SourceSansPro-Light', 12.0),
-                        #('SourceSansPro-Light', 0.0),
-                        ('SourceSansPro-Bold', 12.0),
-                        ('SourceSansPro-Light', 11.249999999999993),
-                        ('SourceSansPro-Bold', 11.249999999999943),
-                        ('SourceSansPro-Light', 11.0),
-                        ('SourceSansPro-Light', 10.999999999999943),
-                        ('SourceSansPro-Light', 10.999999999999986),
-                        ('SourceSansPro-Semibold', 10.0),
-                        ('SourceSansPro-Light', 10.0),
-                        ('SourceSansPro-Light', 11.249999999999943),
-                        ('SourceSansPro-Bold', 11.250000000000007),
-                        '''
-                    ]
-                    for x in to_remove:
+                    for x in self.to_remove:
                         if __name__ == "__main__":
                             pass
                             #print("\t",item._text,x,(item.fontname,item.size))
@@ -139,7 +90,7 @@ class TextConverter(PDFConverter):
         return
 
 def extract_text(pdf_file, password='', page_numbers=None, maxpages=0,
-                 caching=True, codec='utf-8', laparams=None):
+                 caching=True, codec='utf-8', laparams=None,to_remove=[],showpageno=False):
     """Parse and return the text contained in a PDF file.
     :param pdf_file: Either a file path or a file-like object for the PDF file
         to be worked on.
@@ -152,13 +103,15 @@ def extract_text(pdf_file, password='', page_numbers=None, maxpages=0,
         some default settings that often work well.
     :return: a string containing all of the text extracted.
     """
+
+
     if laparams is None:
         laparams = LAParams()
 
     with pdf_file as fp, StringIO() as output_string:
         rsrcmgr = PDFResourceManager()
         device = TextConverter(rsrcmgr, output_string, codec=codec,
-                               laparams=laparams)
+                               laparams=laparams,to_remove=to_remove,showpageno=showpageno)
         interpreter = PDFPageInterpreter(rsrcmgr, device)
 
         for page in PDFPage.get_pages(
@@ -173,11 +126,80 @@ def extract_text(pdf_file, password='', page_numbers=None, maxpages=0,
 
         return output_string.getvalue()
 
-def extract_wrapper(fp):
-    return extract_text(fp,laparams=LAParams(line_margin=2,char_margin=3,boxes_flow=-1))
+def extract_wrapper_outline(fp):
+
+    to_remove = [
+        ('SourceSansPro-Light', 44.0), # Docmenent title
+        ('SourceSansPro-Bold', 18.0), # Document subheading
+        ('SourceSansPro-Regular', 12.0), # Address
+        ('SourceSansPro-Light', 11.25), # Main text
+        ('SourceSansPro-Light', 11.249999999999972), # Extra bits of main text
+        ('SourceSansPro-Light', 11.249999999999993), # Extra main text
+        ('SourceSansPro-Bold', 11.249999999999943), # Extra main text
+        ('SourceSansPro-Light', 11.249999999999943), # Extra main text
+        ('SourceSansPro-Bold', 11.250000000000007), # Extra main text
+        ('SourceSansPro-Regular', 22.0), # "Overall rating for this location"
+        ('SourceSansPro-Regular', 16.0), # Purple thin heading in box (Mental Health Act responsibilities ....)
+        ('SourceSansPro-Regular', 18.0), # Single dash -
+        ('SourceSansPro-Bold', 16.0),# white grey box headings
+        ('SourceSansPro-Light', 12.0),# contents page
+        #('SourceSansPro-Regular', 15.000000000000057),
+        #('SourceSansPro-Regular', 15.0), # Are sevices caring/ responsibvle/safe/well lead... ********
+        #('SourceSansPro-Bold', 13.0), # Rating (big table at top of detailed section) ****
+        ('SourceSansPro-Bold', 14.0), # Specific boxes (e.g. Are surgery services safe?
+        ('SourceSansPro-Light', 11.250000000000014),
+        ('SourceSansPro-Bold', 10.0), # page nums
+        ('SourceSansPro-Regular', 10.0), # footer
+        #('SourceSansPro-Regular', 26.0), # Page Headings ****
+        ('SourceSansPro-Bold', 11.25), # In text headings (e.g. Services we rate)
+        ('SourceSansPro-Bold', 15.0), # some titles, some qurestions - purple headings
+        ('SourceSansPro-Regular', 11.0), # Summary table text
+        ('SourceSansPro-Light', 10.999999999999943), # Extra summary table text
+        ('SourceSansPro-Light', 11.0), # Extra summary table text
+        ('SourceSansPro-Light', 0.0), # Wierdly spaced page headings
+        ('SourceSansPro-Bold', 12.0), # BOLD: Action the provider SHOULD take to improve
+        ('SourceSansPro-Light', 10.999999999999986),
+        ('SourceSansPro-Semibold', 10.0), # Page 16, detailed findings summary table headings
+        ('SourceSansPro-Light', 10.0), # Page 16, detailed findings summary table data
+    ]
+    return extract_text(fp,laparams=LAParams(line_margin=2,char_margin=3,boxes_flow=-1),to_remove=to_remove,showpageno=True)
+
+def extract_wrapper(fp,page_numbers=None):
+    to_remove = [
+        ("SourceSansPro-Regular",10.0),
+        ("SourceSansPro-Bold",10.0),
+        ("SourceSansPro-Bold",13.0),
+        ("SourceSansPro-Bold",14.0),
+        ("SourceSansPro-Bold",14.0),
+        ("SourceSansPro-Bold",15.0),
+        ("SourceSansPro-Regular",12.0),
+        ("SourceSansPro-Semibold",10.0),
+        ("SourceSansPro-Regular",26.0),
+        ("SourceSansPro-Light",44.0),
+        ("SourceSansPro-Bold",15.0),
+        ("SourceSansPro-Light",12.0),
+        ('SourceSansPro-Light', 0.0),
+    ]
+
+    to_remove = [
+        ("SourceSansPro-Regular",10.0),
+        ("SourceSansPro-Bold",10.0),
+        ("SourceSansPro-Bold",13.0),
+        ("SourceSansPro-Bold",14.0),
+        ("SourceSansPro-Bold",14.0),
+        ("SourceSansPro-Bold",15.0),
+        ("SourceSansPro-Regular",12.0),
+        ("SourceSansPro-Semibold",10.0),
+        ("SourceSansPro-Regular",26.0),
+        ("SourceSansPro-Light",44.0),
+        ("SourceSansPro-Bold",15.0),
+        ("SourceSansPro-Light",12.0),
+        ('SourceSansPro-Light', 0.0),
+        ('SourceSansPro-Bold', 18.0),
+    ]
+    return extract_text(fp,laparams=LAParams(line_margin=2,char_margin=3,boxes_flow=-1),to_remove=to_remove,page_numbers=page_numbers)
 
 if __name__ == "__main__":
     text = extract_wrapper(open("AAAH0830.pdf",'rb'))
-    print(re.sub(r"\W+"," ",text.replace("\n"," ")))
+    print(re.sub(r"\W+"," ",text))#.replace("\n"," ")))
     breakpoint()
-    open("test2.txt",'w').write(text)
--- process_pdf.py
+++ process_pdf.py
@@ -1,46 +1,91 @@
-#import html
 import re
-from extractPages import extract_wrapper
+import json
+from extractPages import extract_wrapper,extract_wrapper_outline
+from pathlib import Path
 
 
-data = extract_wrapper(open("AAAH0830.pdf",'rb'))
+def extract(filename):
+    '''Extract text and scores from pdf'''
+    output = {}
+    # *************************************
+    # Pass 1 - Get document structure
+    # *************************************
+    text = extract_wrapper_outline(open(filename,'rb'))
+    text = re.sub("\*\*","",text)
+    text = re.sub("\n[\n\s]+","\n",text)
+    text = re.split("Page \d+",text)
 
-# Get footer
-'''
-footer = data.split("\x0c",1)[0].strip() # get first page
-footer = footer.split("\n")[-1] # last line
-footer = footer[2:] # remove page number]
-#   Make footer regex
-footer_regex = re.compile('\d+\s+'+footer)
-#   Remove instances
-data = footer_regex.sub("",data)
-'''
-data = data.replace("\n\n**","**")
+    #Remove first 2 pages
+    text = text[2:]
+    page_headings = []
+    output = {}
 
-#data = html.unescape(data)
-data = data[data.index("Our ratings for this location are"):]
-data = data[data.index("\x0c"):]
-#data = [d for d in data.split("–––") if "•" in d]
-data = [data]
-for chunk in data[:1]:
-    points = chunk.split("•")
-    for p in points:
-        #print("-----------------------")
-        pSplit = p.split("**",1)
-        if len(pSplit) > 1:
-            p,title = pSplit
-        # titles = re.findall(r'\.\n\n\w+',p)
-        # for t in titles:
-        #     breakpoint()
-        p = p.replace("\n"," ").strip()
-        p = re.sub("\s+"," ",p)
-        print("-",p)
-        if len(pSplit) > 1:
-            title = title[:-2].replace("\n"," ").replace("*","").replace("–","").strip()
-            print("\n\n#",title)
+    # Get ratings
+    for page in text:
+        if page.count("\n") > 5:
+            # Is a ratings table
+            pSplit = page[1:].split("\n")
+            table_index = pSplit.index("Safe")
+            page_heading = " ".join(pSplit[:table_index])
+            pSplit = pSplit[table_index:]
+            pSplit = [p for p in pSplit if p!= ""]
+            questions = pSplit[:int(len(pSplit)/2)]
+            answers = pSplit[int(len(pSplit)/2):]
+
+            questions = [q.strip() for q in questions]
+            answers = [a.strip() for a in answers]
+
+            output[page_heading] = dict(zip(questions,answers))
+            page_headings.append("SECTION")
+        else:
+            # Is a page heading
+            page_headings.append(page.replace("\n"," ").strip())
+
+    # Create map of page numbers
+    page_numbers_map = {}
+    for k in output:
+        page_numbers_map[k] = [i for i,x in enumerate(page_headings) if x == k]
 
 
-# TODO: Idea
-# Make 2 passes
-# Pass 1: Get sections and scores
-# Pass 2: Get text
+    # *************************************
+    # Pass 2 - Get text
+    # *************************************
+    for service in page_numbers_map.keys():
+        print(service,page_numbers_map[service])
+        data = extract_wrapper(open(filename,'rb'),page_numbers=page_numbers_map[service])
+
+        data = data.replace("\n\n**","**")
+        data = [data]
+        service_text = ""
+        for chunk in data:
+            points = chunk.split("•")
+            for p in points:
+                pSplit = p.split("**",1)
+                if len(pSplit) > 1:
+                    p,title = pSplit
+                p = p.replace("\n"," ").strip()
+                p = re.sub("\s+"," ",p)
+                if p != '':
+                    service_text += "- "+p+"\n"
+                if len(pSplit) > 1:
+                    title = title[:-2].replace("\n"," ").replace("*","").replace("–","").strip()
+                    service_text += "\n# "+title+"\n"
+
+        output[service] = {"ratings":output[service],"text":service_text.strip()}
+    return output
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Process pdfs')
+    parser.add_argument('filename', metavar='F', type=str,
+                        help='PDf file')
+
+    args = parser.parse_args()
+    filename = args.filename
+    output_dir = "output"
+    stem = Path(filename).stem
+    data = extract(filename)
+    for k in data:
+        open(output_dir+"/"+stem+"."+k.replace(" ","_")+".txt",'w').write(data[k]["text"])
+        open(output_dir+"/"+stem+"."+k.replace(" ","_")+".labels",'w').write(json.dumps(data[k]["ratings"]))
